ca99dd3b3550c95947d9bcf75e6c11137f1fb7f4
David Gomez Pinto <67220312+Vyper-v@users.noreply.github.com>
2022-02-24 09:26:24 -0300

    Update BurgerMenu.js

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export const BurgerMenu = ({ listItems }) => {
   const [visibility, setVisibility] = useState(window.innerWidth > 768);
