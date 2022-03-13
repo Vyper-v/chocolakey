2bf7c218990b030323b27aef85abd5bfd1dce96d
Vyper-v <davidgomez2924@gmail.com>
2022-03-13 19:22:00 -0300

    back button using useNavigate hook

--- src/components/Item/ItemDetail.js
+++ src/components/Item/ItemDetail.js
@@ -1,12 +1,31 @@
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { ItemCount } from "./ItemCount";
 
 export const ItemDetail = ({ strMeal, strMealThumb, idMeal, price }) => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const myPath = `/item/${idMeal}`;
 
   return (
     <li key={idMeal} className="item">
       <header>
         <img src={strMealThumb} alt={strMeal} loading="lazy" />
-        <ItemCount stock={4}/>
+        <ItemCount />
+        {pathname === myPath ? (
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute top-2 right-5 text-sm font-semibold border-gray-200 text-gray-200"
+          >
+            back
+          </button>
+        ) : (
+          <Link
+            to={myPath}
+            className="absolute top-2 right-5 text-sm text-gray-200"
+          >
+            View more
+          </Link>
+        )}
       </header>
       <footer>
         <span className="item-name">{strMeal}</span>
--- src/components/Item/ItemList.js
+++ src/components/Item/ItemList.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { ItemDetailContainer } from './ItemDetailContainer'
+import React from "react";
+import { ItemDetailContainer } from "./ItemDetailContainer";
 
-export const ItemList = ({data}) => {
+export const ItemList = ({ data }) => {
   return (
     <ul className="[ cluster justify-center ] [ mx-auto ]">
       {data.map((id, i) => (
         <ItemDetailContainer key={i} idMeal={id} />
       ))}
     </ul>
-  )
-}
+  );
+};
